bae2098fb65c73e616662690f554ff5981f07b4c
Rishav Mandal <coder.rishav@gmail.com>
2025-11-02 16:03:46 +0530

    Add redirect from /login/ to /accounts/login/ for convenience

--- MalamalWeekly/urls.py
+++ MalamalWeekly/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin-panel/', include('custom_admin.urls')),  # Custom admin panel
+    path('login/', RedirectView.as_view(url='/accounts/login/', permanent=False)),  # Redirect /login/ to /accounts/login/
     path('', include('games.urls')),
     path('accounts/', include('accounts.urls')),
     path('transactions/', include('transactions.urls')),
